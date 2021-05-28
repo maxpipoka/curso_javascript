807a533056a302d486e385e66cdd14da2b902beb
maxpipoka <martincardozo@gmail.com>
2021-05-28 11:47:32 -0300

    Primer commit

---
+++ ejercicio02.js
@@ -0,0 +1,1 @@
+// 2 - Escribe un programa que pueda calcular el área de 3 figuras geométricas, triángulo, rectángulo y círculo. En primer lugar pregunta de qué figura se quiere calcular el área, después solicita los datos que necesites para calcularlo. triángulo = b * h/2 rectángulo = b * h círculo = π * r2 (pi * radio al cuadrado)
