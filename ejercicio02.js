51c46282f0a3467fd3f7c02fc5d413b38e3bce9d
maxpipoka <martincardozo@gmail.com>
2021-05-31 01:26:31 -0300

    Terminado el ejercicio 2

--- ejercicio02.js
+++ ejercicio02.js
@@ -1,1 +1,24 @@
 // 2 - Escribe un programa que pueda calcular el área de 3 figuras geométricas, triángulo, rectángulo y círculo. En primer lugar pregunta de qué figura se quiere calcular el área, después solicita los datos que necesites para calcularlo. triángulo = b * h/2 rectángulo = b * h círculo = π * r2 (pi * radio al cuadrado)
+
+let figura;
+
+while (figura != "A" && figura != "B" && figura != "C") {
+    figura = prompt("De que figura quieres calcular el área? A-Triángulo, B-Rectángulo y C-Círculo");
+}
+
+if (figura == "A") {
+    let ancho = prompt("Ingrese el ancho del triángulo:");
+    let alto = prompt("Ingrese el alto del triángulo");
+    console.log(`El área del triángulo con ancho de ${ancho} y altura de ${alto} propuesto es ${ (ancho * alto) / 2}`);
+}
+
+if (figura == "B") {
+    let ancho = prompt("Ingrese el ancho del rectángulo:");
+    let alto = prompt("Ingrese el alto del rectángulo");
+    console.log(`El área del rect+angulo con ancho de ${ancho} y altura de ${alto} propuesto es ${ (ancho * alto)}`);
+}
+
+if (figura == "C") {
+    let diametro = prompt("Ingrese el diámetro del circulo:");
+    console.log(`El área del circulo propuesto con un diámetro de ${diametro} es ${ Math.PI * (((diametro / 2) * (diametro / 2)))}`);
+}
---
+++ ejercicio03.js
@@ -0,0 +1,7 @@
+// 3 - Solicita un número e imprime todos los números pares e impares desde 1 hasta ese número con el mensaje "es par" o "es impar" si el número es 5 el resultado será: 1 - es impar 2 - es par 3 - es impar 4 - es par 5 - es impar
+
+let numerofinal = prompt("Ingrese el numero final: ");
+
+for (i = 0, i = numerofinal, i++) {
+
+}
