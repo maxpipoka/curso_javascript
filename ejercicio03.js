275c86dbc4f3721251df49c43e039d03ebd79ebb
maxpipoka <martincardozo@gmail.com>
2021-05-31 09:41:18 -0300

    Iniciado el ejercicio 4

--- ejercicio03.js
+++ ejercicio03.js
@@ -1,7 +1,14 @@
 // 3 - Solicita un número e imprime todos los números pares e impares desde 1 hasta ese número con el mensaje "es par" o "es impar" si el número es 5 el resultado será: 1 - es impar 2 - es par 3 - es impar 4 - es par 5 - es impar
 
-let numerofinal = prompt("Ingrese el numero final: ");
+let numerofinal = parseInt(prompt("Ingrese el numero final: "));
+console.log(numerofinal);
+console.log(numerofinal + numerofinal);
 
-for (i = 0, i = numerofinal, i++) {
-
+for (let i = 1; i <= numerofinal; i++) {
+    if (i % 2 == 0) {
+        console.log(`El numero ${i} es par.`)
+    }
+    if (i % 2 != 0) {
+        console.log(`El numero ${i} es impar.`)
+    }
 }
---
+++ ejercicio04.js
@@ -0,0 +1,22 @@
+// 4 - Escribe un programa que pida un número entero mayor que 1 y que escriba si el número primo o no. Un número primo es aquel que solo es divisible por sí mismo y la unidad
+
+let numeroAEvaluar = parseInt(prompt("Ingrese el valor a evaluar si es primo o no: "));
+
+let esPrimo = true;
+let dividendos = [2, 3, 4, 5, 6, 7, 8, 9]
+
+for (let i of dividendos) {
+    if (numeroAEvaluar % i == 0) {
+        console.log(`Div evaluado ${i}`);
+        esPrimo = false;
+        break;
+    }
+}
+
+if (esPrimo === true) {
+    console.log(`El numero ${numeroAEvaluar} es un numero primo.`);
+}
+
+if (esPrimo === false) {
+    console.log(`El numero ${numeroAEvaluar} NO es un numero primo.`);
+}
