3fa1f495ad98e8065b973208697bf9b1981984cb
maxpipoka <martincardozo@gmail.com>
2021-06-01 16:30:59 -0300

    Terminado el ejercicio 6

--- ejercicio06.js
+++ ejercicio06.js
@@ -1,1 +1,21 @@
-// 6 - Escribe un programa que permita ir introduciendo una serie indeterminadade números mientras su suma no supere 50. Cuando esto ocurra, se debe mostrar el total acumulado y el contador de cuantos números se han introducido
+// 6 - Escribe un programa que permita ir introduciendo una serie indeterminada de números mientras su suma no supere 50. Cuando esto ocurra, se debe mostrar el total acumulado y el contador de cuantos números se han introducido
+
+let total = 0;
+let contador = 0;
+let nTemporal = 0;
+
+while (total <= 50) {
+    nTemporal = parseInt(prompt("Introduzca numeros a sumar: "));
+    if ((total + nTemporal) > 50) {
+        console.log(`ATENCION! No se pueden sumar mas numeros`);
+        break;
+    }
+
+    if ((total + nTemporal) < 51) {
+        total += nTemporal;
+        contador += 1;
+        console.log(`${total}`);
+    }
+}
+console.log(`El total guardado fue de ${total}.`);
+console.log(`Y se guardaron ${contador} numeros.`);
